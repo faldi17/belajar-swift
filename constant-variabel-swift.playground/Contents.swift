1d7f053005eee6f713578d5fd85f098fdd6e2ec8
Faldiansyah <faldiansyahk@gmail.com>
2025-01-02 21:23:18 +0800

    initial commit

---
+++ constant-variabel-swift.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var iniAngka = 1234 // nilai bisa berubah
+let iniConstant = 10 // nilai tetap
+
+iniAngka = 123
+
+print(iniAngka)
+print(iniConstant)
+
+var p = 30, l = 20, t = 25 // menulis var singkat
+print(p, l, t)
+
+var kalimat : String
+kalimat = "Semangat Belajarnya Ayang... :v "
+print(kalimat)
---
+++ helloworld-swift.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var angka = 123
+
+print("Hello World!")
+print(greeting)
+print(1234)
+print(angka)
